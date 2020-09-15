0d50360984b7b02f3cdcc06376e7e79777c7e980
wizarlock <shamaro.rv@edu.spbstu.ru>
2020-09-15 21:00:30 +0300

    Lesson 2 task 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,8 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -20,6 +22,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  */
 fun isNumberHappy(number: Int): Boolean =
     (number / 1000 % 10 + number / 100 % 10) == (number / 10 % 10 + number % 10)
+
 /**
  * Простая (2 балла)
  *
@@ -27,8 +30,8 @@ fun isNumberHappy(number: Int): Boolean =
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
-
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)
 
 /**
  * Простая (2 балла)
@@ -36,7 +39,14 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    return if (year % 4 == 0 && month == 2 && year % 100 != 0 || year % 400 == 0) 29
+    else when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        2 -> 28
+        else -> 30
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -48,7 +58,9 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean =
+    sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2
+
 
 /**
  * Средняя (3 балла)
@@ -59,4 +71,6 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+    (a <= s && b <= r) || (a <= r && b <= s) || (c <= r && a <= s) || (c <= r && b <= s) || (c <= s && a <= r) ||
+            (c <= s && b <= r)
